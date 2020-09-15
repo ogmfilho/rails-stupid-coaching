6322397054f83dd90262bd4fada132b4850c9815
Otoniel Miranda <ogmfilho@gmail.com>
2020-09-15 16:05:43 -0300

    rails d1.2 ok

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,25 @@
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @question = params[:question]
+
+    # raise
+
+    @answer = if @question == 'I am going to work'
+                'Great!'
+              elsif @question.last == '?'
+                'Silly question, get dressed and go to work!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
+
+    # if @question == 'I am going to work'
+    #   @answer = 'Great!'
+    # elsif @question.last == '?'
+    #   @answer = 'Silly question, get dressed and go to work!'
+    # else
+    #   @answer = "I don't care, get dressed and go to work!"
+    # end
+  end
+end
